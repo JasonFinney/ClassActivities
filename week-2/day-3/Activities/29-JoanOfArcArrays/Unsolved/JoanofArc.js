3e0a73c153e585f60e91e5336dff37190e501bf2
Jason Finney <finney.jason.218@gmail.com>
2018-08-06 15:09:36 -0500

    week3 day1 activities

---
+++ week-2/day-3/Activities/29-JoanOfArcArrays/Unsolved/JoanofArc.js
@@ -0,0 +1,13 @@
+var joanOfArchInfo = {
+    key: value,
+    key1: value1,
+    key2: value2,
+}
+
+var joanOfArchInfo = {
+    "Real Name": "Jehanne la Pucelle.",
+    "Grew Up Where": "Domremy, a village in northeastern France.",
+    "Known For": "Peasant girl, daughter of a farmer, who rose to become Commander of the French army.",
+    "Scars": "Took an arrow to the shoulder and a crossbow bolt to the thigh while trying to liberate Paris.",
+    "Symbolism": "Stands for French unity and nationalism.",
+}
