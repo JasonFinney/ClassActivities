d2d73d85fa0da8a654ae2d969354cb4591eb7b62
Jason Finney <finney.jason.218@gmail.com>
2018-09-26 14:38:16 -0500

    stuff/n/things

--- week-9/day-4/Activities/11-Scraping-into-a-db/inclass1/server.js
+++ week-9/day-4/Activities/11-Scraping-into-a-db/inclass1/server.js
@@ -22,12 +22,12 @@ var collections = ["scrapedData"];
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
+db.on("error", function (error) {
   console.log("Database Error:", error);
 });
 
 // Main route (simple Hello World Message)
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Hello world");
 });
 
@@ -38,7 +38,15 @@ app.get("/", function(req, res) {
 // This route will retrieve all of the data
 // from the scrapedData collection as a json (this will be populated
 // by the data you scrape using the next route)
-
+app.get("/grabdata", function (req, res) {
+  db.scrapedData.find({}, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
 // Route 2
 // =======
 // When you visit this route, the server will
@@ -47,10 +55,21 @@ app.get("/", function(req, res) {
 // TIP: Think back to how you pushed website data
 // into an empty array in the last class. How do you
 // push it into a MongoDB collection instead?
-
+app.get("/scrape", function (req, res) {
+  request("https://old.reddit.com/r/webdev/", function (error, response, html) {
+    var $ = cheerio.load(html);
+    var results = [];
+    $("p.title").each(function (i, element) {
+      var title = $(element).text();
+      var link = $(element).children().attr("href");
+      db.scrapedData.insert({ "title": title, "link": link });
+    });
+    console.log(results);
+  });
+});
 /* -/-/-/-/-/-/-/-/-/-/-/-/- */
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("App running on port 3000!");
 });
