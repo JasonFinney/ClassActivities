eb0677ca003cdedeb46562de186e1cab84277c26
Jason Finney <finney.jason.218@gmail.com>
2018-09-27 13:28:51 -0500

    absolutely no reason to do this

--- week-9/day-5/Activities/18-Intro-Populate/inclass1/models/Library.js
+++ week-9/day-5/Activities/18-Intro-Populate/inclass1/models/Library.js
@@ -1,8 +1,5 @@
 var mongoose = require("mongoose");
-
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
-
 // Using the Schema constructor, create a new LibrarySchema object
 // This is similar to a Sequelize model
 var LibrarySchema = new Schema({
