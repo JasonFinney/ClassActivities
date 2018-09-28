4114db88be74066eed5d9662d93fb80d982bbaaa
Jason Finney <finney.jason.218@gmail.com>
2018-09-28 12:53:36 -0500

    react is a Godsend

--- week-10/day-2/Activities/07-Stu_JSXVariables/reactpractice/src/components/JSXVariables.js
+++ week-10/day-2/Activities/07-Stu_JSXVariables/reactpractice/src/components/JSXVariables.js
@@ -1,15 +1,23 @@
 import React from "react";
 
-const JSXVariables = () => (
-  <div className="main-container">
-    <div className="container">
-      <div className="jumbotron">
-        <h1>Hi! My name is Jason Finney</h1>
-        <h2>My name has  letters</h2>
-        <h2>I think React </h2>
+const JSXVariables = () => {
+  const name = "Jason Finney";
+  const num1 = 5;
+  const num2 = 10;
+  var randnum = Math.floor(Math.random() * 10);
+  return (
+    <div className="main-container">
+      <div className="container">
+        <div className="jumbotron">
+          <h1>Hi! My name is {name}</h1>
+          <h2>My name has {name.length - 1} letters</h2>
+          <h2>I think React is a Godsend</h2>
+          <h2>Also here is a random number: {randnum}</h2>
+          <h2>Here is that number multiplied by 5! - {randnum * num1}</h2>
+          <h2>Here is that number multiplied by 10! - {randnum * num2}</h2>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default JSXVariables;
