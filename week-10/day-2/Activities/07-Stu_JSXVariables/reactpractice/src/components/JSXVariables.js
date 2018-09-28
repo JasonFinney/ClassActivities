5fc0dec8353c464da6569f10528b28fef1240602
Jason Finney <finney.jason.218@gmail.com>
2018-09-28 12:43:43 -0500

    the react is the reatest

--- week-10/day-2/Activities/07-Stu_JSXVariables/reactpractice/src/components/JSXVariables.js
+++ week-10/day-2/Activities/07-Stu_JSXVariables/reactpractice/src/components/JSXVariables.js
@@ -4,9 +4,9 @@ const JSXVariables = () => (
   <div className="main-container">
     <div className="container">
       <div className="jumbotron">
-        <h1>Hi! My name is (insert name here)</h1>
-        <h2>My name has (insert number of letters in name here) letters</h2>
-        <h2>I think React (insert thoughts about React here)</h2>
+        <h1>Hi! My name is Jason Finney</h1>
+        <h2>My name has  letters</h2>
+        <h2>I think React </h2>
       </div>
     </div>
   </div>
