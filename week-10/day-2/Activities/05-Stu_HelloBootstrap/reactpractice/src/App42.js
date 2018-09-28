a922da0bf7f3c411264f8e49596c5bd1b09b3c3c
Jason Finney <finney.jason.218@gmail.com>
2018-09-28 11:41:44 -0500

    nothing to see here

--- week-10/day-2/Activities/05-Stu_HelloBootstrap/reactpractice/src/App42.js
+++ week-10/day-2/Activities/05-Stu_HelloBootstrap/reactpractice/src/App42.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const App = () => (
+const App42 = () => (
     < div className="container" >
         <div className="row">
             <div className="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
+                <h1 class="display-4">Look another component thingy!</h1>
                 <a class="btn btn-primary btn-lg" href="./other.html" role="button">Learn more</a>
             </div>
         </div>
     </div >
 );
-export default App;
+export default App42;
--- week-10/day-2/Activities/05-Stu_HelloBootstrap/reactpractice/src/index.js
+++ week-10/day-2/Activities/05-Stu_HelloBootstrap/reactpractice/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import App42 from "./App42";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App42 />, document.getElementById("root42"));
