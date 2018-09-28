6f46a2b8b20822ac716fe544d72de5d77f28b4d6
Jason Finney <finney.jason.218@gmail.com>
2018-09-28 14:36:11 -0500

    nope, nothing, move along

--- week-10/day-2/Activities/11-Stu_PropsCalculator/reactpractice/src/components/Calculator.js
+++ week-10/day-2/Activities/11-Stu_PropsCalculator/reactpractice/src/components/Calculator.js
@@ -1,23 +1,30 @@
 import React from "react";
+import Math from "./Math";
 
 // Create a new component named "Math"
 // Render one Math component in the place of each "?" mark
 // Math should accept 3 props
 // num1, operator, and num2
 // Math should return a span tag displaying the result e.g.  19 + 341 = 360
-const Calculator = () => (
+const Calculator = (props) => (
   <div>
     <p>
-      19 + 341 = ?
+      19 + 341 = {19 + 341}
     </p>
     <p>
-      42 - 17 = ?
+      42 - 17 = {42 - 17}
     </p>
     <p>
-      100 * 3 = ?
+      100 * 3 = {100 * 3}
     </p>
     <p>
-      96 / 4 = ?
+      96 / 4 = {96 / 4}
+    </p>
+
+
+
+    <p>
+      {props.num1} {props.operator} {props.num2}
     </p>
   </div>
 );
