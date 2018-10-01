1f973bcbfff3aa0fb7bf1f574e4a901225d487eb
Jason Finney <finney.jason.218@gmail.com>
2018-10-01 11:30:00 -0500

    react app demo

--- week-10/day-2/Activities/11-Stu_PropsCalculator/reactpractice/src/components/Math.js
+++ week-10/day-2/Activities/11-Stu_PropsCalculator/reactpractice/src/components/Math.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Math = (props) => {
+    console.log(props);
     return (
-        
-    )
-}
+        <div className={`math math-${props.type || ""}`}>
+
+        </div>
+    );
+};
+
+export default Math;
