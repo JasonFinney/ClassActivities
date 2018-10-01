c15f9ae2a23bda6f372378f05404aff77cabbee3
Jason Finney <finney.jason.218@gmail.com>
2018-10-01 14:31:52 -0500

    statestuff5

--- week-10/day-2/Activities/14-Ins_BasicState/statestuff/src/components/Counter.js
+++ week-10/day-2/Activities/14-Ins_BasicState/statestuff/src/components/Counter.js
@@ -12,6 +12,10 @@ class Counter extends React.Component {
     // We always use the setState method to update a component's state
     this.setState({ count: this.state.count + 1 });
   };
+  handleDecrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: this.state.count - 1 });
+  };
 
   // The render method returns the JSX that should be rendered
   render() {
@@ -24,6 +28,9 @@ class Counter extends React.Component {
           <p className="card-text">Click Count: {this.state.count}</p>
           <button className="btn btn-primary" onClick={this.handleIncrement}>
             Increment
+          </button>
+          <button className="btn btn-primary" onClick={this.handleDecrement}>
+            Decrement
           </button>
         </div>
       </div>
