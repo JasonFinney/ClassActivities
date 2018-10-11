1d50306018dca3e3098e79de8b397a50d7f214c6
Jason Finney <finney.jason.218@gmail.com>
2018-10-11 05:10:49 -0500

    axios!

--- week-10/day-3/Activities/18-Stu_FunWithForms/inclass1/funwithforms/src/components/Form/Form.js
+++ week-10/day-3/Activities/18-Stu_FunWithForms/inclass1/funwithforms/src/components/Form/Form.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./Form.css";
 
 class Form extends Component {
-  // Setting the component's initial state
   state = {
     firstName: "",
     lastName: "",
@@ -10,30 +9,33 @@ class Form extends Component {
   };
 
   handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const value = event.target.value;
+    let value = event.target.value;
     const name = event.target.name;
-
-    // Updating the input's state
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
     this.setState({
       [name]: value
     });
   };
-
   handleFormSubmit = event => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     event.preventDefault();
-
+    if (!this.state.firstName || !this.state.lastName) {
+      alert("Please enter a first and last name!");
+    } else if (this.state.password.length < 6) {
+      alert("Please choose a longer password, " + this.state.firstName + " " + this.state.lastName + "!");
+    } else {
+      alert("Hello, " + this.state.firstName + " " + this.state.lastName + "!");
+    }
     // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
-    alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
     this.setState({
       firstName: "",
-      lastName: ""
+      lastName: "",
+      password: ""
     });
   };
-
   render() {
-    // Notice how each input has a `value`, `name`, and `onChange` prop
     return (
       <div>
         <p>
@@ -53,6 +55,13 @@ class Form extends Component {
             onChange={this.handleInputChange}
             type="text"
             placeholder="Last Name"
+          />
+          <input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
           />
           <button onClick={this.handleFormSubmit}>Submit</button>
         </form>
